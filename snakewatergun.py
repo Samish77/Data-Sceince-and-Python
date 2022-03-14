bcec4421d41082127b85f2a46c722f38fbee5191
Samish77 <99634815+Samish77@users.noreply.github.com>
2022-03-14 09:53:41 +0530

    Python code for game

---
+++ snakewatergun.py
@@ -0,0 +1,40 @@
+import random
+# Sanke water gun Game
+def gameWin(comp,you): 
+    if comp==you:
+        return None
+    elif comp=='s':
+        if you=='w':
+            return False
+        elif you=='g':
+            return True
+    elif comp=='w':
+        if you=='g':
+            return False
+        elif you=='s':
+            return True
+    elif comp=='g':
+        if you=='s':
+            return False
+        elif you=='w':
+            return True
+
+print("comp turn: snake(s) water(w) or gun(g)   ?")
+randNo=random.randint(1,3)
+if randNo==1:
+    comp='s'
+elif randNo==2:
+    comp='w'
+elif randNo=='3':
+    comp='g'
+
+you =input("your turn:comp turn: snake(s) water(w) or gun(g)   ?")        
+a=gameWin(comp,you)
+print(f"computer chose {comp}")
+print(f"you chose {you}")
+if a==None:
+    print("the game is a tie")
+elif a:
+    print("you won")
+else:
+    print("you lose")
